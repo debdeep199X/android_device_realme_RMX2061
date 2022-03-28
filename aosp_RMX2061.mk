30cd90ff79e22e457edbb4e7c03b0b1d236c84ce
debdeep199X <debdeep199x@gmail.com>
2022-03-28 05:25:26 +0000

    RMX2061: Toggle support for quick tap.

--- aosp_RMX2061.mk
+++ aosp_RMX2061.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/realme/RMX2061/device.mk)
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# Target
+TARGET_SUPPORTS_QUICK_TAP := true
+
 # Inherit some common AOSP stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
@@ -24,8 +27,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="atoll-user 11 RKQ1.200903.002/ eng.root.04080114.013112 release-keys"
-
-BUILD_FINGERPRINT := "google/raven/raven:12/SD1A.210817.015.A4/7697517:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX2061" \
